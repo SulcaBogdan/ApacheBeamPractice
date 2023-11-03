b23b0de7a67a597f8426f2ca2bfe8ff5b08a7cf8
SulcaBogdan <stefanbogdansulca@gmail.com>
2023-11-03 12:38:02 +0200

    Theory

---
+++ src/main/java/ApacheLearning.java
@@ -0,0 +1,48 @@
+public class ApacheLearning {
+    //Teorie
+
+    /*Apache beam este un model unificat pentru definirea fluxurilor de prelucrare/procesare a datelor batch sau streaming.
+
+    Acesta este alcatuit din:
+        -> Pipeline -> este un graph construit de user care arata cum vor fi procesate datele.
+
+        -> PCollection -> este un data set sau un data stream. Datele pe care un 'Pipeline' le proceseaza fac parte
+        dintr-un Pcollection.
+
+        -> PTransform -> este un pas sau o actiune in Pipeline. O transformare este aplicata la zero sau mai multe obiecte
+         PCollection si genereaza altele noi.
+        PCollection este imutabila asadar pentru a modifica sau 'transforma' datele dintr un PCollection se va crea o noua
+         copie a PCollection cu transformarile de rigoare.
+
+        -> Aggregation -> inseamna sa faci un calcul folosind informatii din mai multe surse.
+
+        -> User-defined function (UDF) -> cod scrie de user care poate fi folosit pentru a configura modul in care se
+        efectueaza o transformare.
+
+        -> Schema -> pentru un PCollection defineste elementele acelei PCollection ca o lista ordonata de campuri denumite
+
+        -> SDK -> este o biblioteca speciala pentru anumite limbaje de programare, care ajuta dezvoltatorii sa creeze
+        transformari, sa construiasca pipeline-uri si sa le trimita catre un "runner"(executor).
+
+        -> Runner -> un runner ruleaza un pipeline Beam folosind capacitatile motorului de prelucrare a datelor pe care l-ai ales.
+
+        -> Window -> Un PCollection poate fi impartit pe windows(ferestre) in functie de marcajele temporale ale elementelor
+        in functie de marcajele temporale ale elementelor individuale.
+
+        -> Watermark -> reprezinta o estimare a momentului in care se preconizeaza ca toate datele dintr-o anumita fereastra
+         sunt asteptate sa ajunga. Acest lucru este necesar deoarece datele nu sunt intotdeauna garantate sa ajunga intr-un pipline
+         in ordine cronologica sau sa ajunga intotdeauna la intervale previzibile.
+
+        -> Trigger -> stabileste momentul cand sa agregam rezultatele fiecarei ferestre.
+
+        -> State and timers -> sunt instrumente de baza care iti ofera control total asupra modului in care agreghezi colectiile
+        de intrare care se extind in timp, in functie de chei.
+
+        -> Splittable DoFn -> permite procesarea elementelor intr-un mod care nu este momolitic(bloc mare de cod).Poti face
+        checkpoint la procesarea unui element, iar executorul poate imparti restul lucrarii pentru a face mai multe lucruri in paralel.
+
+     */
+
+
+
+}
